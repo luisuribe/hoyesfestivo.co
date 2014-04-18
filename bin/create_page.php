8d571040fc4a02033aae2eacc4f4e2bc4619b8b5
Luis Uribe <acme@eviled.org>
2014-04-17 20:55:43 -0500

    Adding counter

--- bin/create_page.php
+++ bin/create_page.php
@@ -1,16 +1,23 @@
 <?php 
-$db = new SQLite3('./holidays');
-$now = date('Y-m-d');
-$results = $db->query("SELECT * FROM holidays WHERE holiday_date >= '".$now."' LIMIT 1");
-$row = $results->fetchArray();
+$db      = new SQLite3('./holidays');
+$now     = new DateTime(date('Y-m-d'));
+$results = $db->query("SELECT * FROM holidays WHERE holiday_date >= '".$now->format('Y-m-d')."' LIMIT 1");
+$row     = $results->fetchArray();
 
-if ( $now == $row['holiday_date'] ) {
+$holiday_date = new DateTime($row['holiday_date']);
+
+if ( $now->format('Y-m-d') == $holiday_date->format('Y-m-d') ) {
     copy('../templates/holiday.html', '../index.html');
 } else {
+    $difference = $now->diff($holiday_date);
+
+    $note = "Faltan <strong>X</strong> d&iacute;as para el pr&oacute;ximo festivo (HOLYDAY)";
+    $note = preg_replace('/HOLYDAY/', $row['holiday_date'] , $note);
+    $note = preg_replace('/X/', $difference->days , $note);
+
     $template = file_get_contents('../templates/not_holiday.html');
-    $new_file = preg_replace('/HOLYDAY/', $row['holiday_date'], $template);
-    $fp = fopen('../index.html', 'w+');
+    $new_file = preg_replace('/NOTE/', $note , $template);
+    $fp       = fopen('../index.html', 'w+');
     fwrite($fp, $new_file);
     fclose($fp);
 }
-?>
